bb8f1cbd876acba240c65c917f1c7b037d5a1099
verma-gunjan <vermagunjan9927@gmail.com>
2025-05-31 16:14:21 +0000

    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,5 @@ class Book < ApplicationRecord
     has_many :categories, through: :book_categories
     has_many :recommendations
     has_one_attached :book_image
+    validates :title, :author, presence: true
 end
