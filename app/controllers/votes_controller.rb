58987f30ead140737a2d805f06e8189303bd79a1
verma-gunjan <vermagunjan9927@gmail.com>
2025-05-31 15:59:43 +0000

    add real time vote update broadcasting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,17 @@
 class VotesController < ApplicationController
   before_action :authenticate_user!
   def create
-    recommendation = Recommendation.find(params[:recommendation_id])
-    vote = recommendation.votes.build(user: current_user)
-
-    if vote.save
-      redirect_back fallback_location: root_path, notice: "Vote added!"
-    else
-      redirect_back fallback_location: root_path, alert: "You already voted."
+    @recommendation = Recommendation.find(params[:recommendation_id])
+    @vote = @recommendation.votes.build(user: current_user)
+  
+    respond_to do |format|
+      if @vote.save
+        format.turbo_stream
+        format.html { redirect_back fallback_location: root_path, notice: "Voted!" }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("vote_flash_#{@recommendation.id}", partial: "votes/error", locals: { recommendation: @recommendation }) }
+        format.html { redirect_back fallback_location: root_path, alert: "You already voted." }
+      end
     end
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,10 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :recommendation
   validates :user_id, uniqueness: { scope: :recommendation_id }
+  after_create_commit do
+    broadcast_replace_to "recommendations",
+      target: "vote_count_#{recommendation.id}",
+      partial: "recommendations/vote_count",
+      locals: { recommendation: recommendation }
+  end
 end
