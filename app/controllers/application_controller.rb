dfd9b06ef0be64a263983da62fe01630e028815d
verma-gunjan <vermagunjan9927@gmail.com>
2025-06-01 06:33:10 +0000

    fix ui for auth and handle root route for nonauth user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :profile_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :profile_image])
+  end
 
   # def after_sign_in_path_for(resource)
   #   recommendations_path
--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,5 +1,5 @@
 class RecommendationsController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def new
     @recommendation = Recommendation.new
